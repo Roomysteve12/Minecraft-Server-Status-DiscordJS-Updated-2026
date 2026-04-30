8bd419529d9d1cf4c79160b2864f638acaa6dcbf
Leigh Ivan Corre <54193494+Roomysteve12@users.noreply.github.com>
2026-04-30 08:45:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 const config = require('./config.json')
 const software = ['java', 'bedrock']
 if(!config.token || !config.software || !config.ip || !software.includes(config.software)) {
-  return console.log(`Go To config.json file! \nToken: ${config.token || 'private'}\nSoftware: ${config.software || 'bedrock/java'}\nIp And Port: ${config.ip || 'play.example.net'}:${config.port || '19132/25565'}`.red)
+  return console.log(`Go To config.json file! \nToken: ${config.token || 'private'}\nSoftware: ${config.software || 'bedrock'}\nIp And Port: ${config.ip || 'play.example.net'}:${config.port || '19132'}`.red)
 } else {
  console.log('Successfully!'.green)
 }
@@ -16,7 +16,7 @@ if(config.software === 'java') {
 }
 
 // Main JS
-const { Client, GatewayIntentBits, Partials, Collection, PermissionFlagsBits } = require('discord.js')
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js')
 const Discord = require('discord.js')
 const client = new Client({
   intents: [
@@ -42,7 +42,7 @@ client.slash = new Discord.Collection();
   require(`./handlers/${handler}`)(client);
 });
 
-client.on('ready', async () => {
+client.on('clientReady', async () => {
 
   // changing status
   setIntervalAsync(
@@ -51,39 +51,29 @@ client.on('ready', async () => {
     if(config.software === 'bedrock') {
       const fetch = require('node-fetch')
 
-      let { debug, motd, players, online } = await fetch(`https://api.mcsrvstat.us/bedrock/3/${config.ip}:${config.port}`).then(response => response.json())
-      if(debug.query === false) {
-      client.user.setPresence({ activities: [{ name: `Status: Offline` }]  })
-      }
+      let { motd, players, online } = await fetch(`https://api.mcstatus.io/v2/status/bedrock/${config.ip}:${config.port}`).then(response => response.json())
 
       if(!online && online === false) {
         client.user.setPresence({ activities: [{ name: `Status: Offline` }]  })
       }
 
       if(online === true) {
-        client.user.setPresence({ activities: [{ name: `${motd.raw} | Status: Online | Players: ${players.online}/${players.max}` }]  })
-
-  if(motd.raw.length === "24") client.user.setPresence({ activities: [{ name: `Bedrock Server | Status: Online | Players: ${players.online}/${players.max}` }]  })      
+        client.user.setPresence({ activities: [{ name: `Bedrock Server | Status: Online | Players: ${players.online}/${players.max}` }]  })     
       }
     }
 
     if(config.software === 'java') {
       const fetch = require('node-fetch')
 
-      let { debug, motd, players, online } = await fetch(`https://api.mcsrvstat.us/3/${config.ip}:${config.port}`).then(response => response.json())
-      if(debug.ping === false) {
-      client.user.setPresence({ activities: [{ name: `Status: Offline` }]  })
-      }
+      let {motd, players, online } = await fetch(`https://api.mcstatus.io/v2/status/java/${config.ip}:${config.port}`).then(response => response.json())
 
       if(!online && online === false) {
         client.user.setPresence({ activities: [{ name: `Status: Offline` }]  })
       }
 
-      if(online === true) {
+      if(online === true) {    
 
-  if(motd.raw.length === "24") client.user.setPresence({ activities: [{ name: `Java Server | Status: Online | Players: ${players.online}/${players.max}` }]  })      
-
-        client.user.setPresence({ activities: [{ name: `Status: Online | Players: ${players.online}/${players.max}` }]  })
+        client.user.setPresence({ activities: [{ name: `Java Server | Status: Online | Players: ${players.online}/${players.max}` }]  })
       }
     }
 
@@ -97,8 +87,8 @@ client.on('ready', async () => {
     if(config.software === 'bedrock') {
       const fetch = require('node-fetch')
 
-      let { debug, motd, players, online } = await fetch(`https://api.mcsrvstat.us/bedrock/3/${config.ip}:${config.port}`).then(response => response.json());
-      if(debug.query === false) {
+      let { motd, players, online } = await fetch(`https://api.mcstatus.io/v2/status/bedrock/${config.ip}:${config.port}`).then(response => response.json());
+      if(online === false) {
       client.user.setPresence({ activities: [{ name: `Status: Offline` }]  })
       }
 
@@ -111,7 +101,7 @@ client.on('ready', async () => {
       }
 
       if(online === true) {
-        if(uptime > 100) return;
+        if(uptime > 99) return;
         if(!uptime) db.set('up', 100)
         if(uptime) db.add('up', 1)
       }
@@ -120,8 +110,8 @@ client.on('ready', async () => {
     if(config.software === 'java') {
       const fetch = require('node-fetch')
 
-      let { debug, motd, players, online } = await fetch(`https://api.mcsrvstat.us/3/${config.ip}:${config.port}`).then(response => response.json());
-      if(debug.ping === false) {
+      let { motd, players, online } = await fetch(`https://api.mcstatus.io/v2/status/java/${config.ip}:${config.port}`).then(response => response.json());
+      if(online === false) {
       client.user.setPresence({ activities: [{ name: `Status: Offline` }]  })
       }
       let uptime = db.get('up')
@@ -133,12 +123,14 @@ client.on('ready', async () => {
       }
 
       if(online === true) {
-        if(uptime > 100) return;
+        if(uptime > 99) return;
         if(!uptime) db.set('up', 100)
         if(uptime) db.add('up', 1)
       }
     }
   }, 60000)
+
+ 
 })
 
 client.on('interactionCreate', async (inter) => {
@@ -146,7 +138,7 @@ client.on('interactionCreate', async (inter) => {
   if(!inter.isButton) return;
   const cmd = client.slash.get(inter.commandName);
   if(!cmd) return;
-
+   
   const role = config.role
   if(role) {
   const only = inter.member.roles.cache.has(role)
