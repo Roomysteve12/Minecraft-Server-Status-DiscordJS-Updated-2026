24eafd43deed9dc6707b9b3ea5c1df431ef1436d
Leigh Ivan Corre <54193494+Roomysteve12@users.noreply.github.com>
2026-04-30 08:46:17 +0800

    Update mcstatus.js
    new api and latency ms checker

--- slash/minecraft/mcstatus.js
+++ slash/minecraft/mcstatus.js
@@ -8,11 +8,15 @@ run: async (client, inter, config, db) => {
 
   let uptime = db.get('up')
 
+  const start = Date.now();
+
   if(config.software === 'bedrock') {
    let { motd, players, online, version } = await fetch(`https://api.mcsrvstat.us/bedrock/3/${config.ip}:${config.port}`).then(response => response.json())
 
    let player = players
    if(!player) player = "null"
+   
+   let ping = Date.now() - start;
 
     if(online === false) {
       let embed = new Discord.EmbedBuilder()
@@ -33,9 +37,10 @@ run: async (client, inter, config, db) => {
       .setThumbnail(inter.guild.iconURL())
       .setTitle(`${config.emoji || ":skull:"} | ${inter.guild.name}'s Status`)
       .addFields({ name: 'Status: ', value: ':green_circle: Online', inline: true })
-      .addFields({ name: 'Players/Max: ', value: `${player.online}/${player.max}` })
-      .addFields({ name: 'Version: ', value: `${version}`, inline: true })
-      .addFields({ name: 'Uptime (Not Accurate): ', value: `${uptime}%`, inline: true })
+      .addFields({ name: 'Players/Max: ', value: `${player.online}/${player.max}`, inline: true })
+      .addFields({ name: 'Version: ', value: `${version}`, inline: true})
+      .addFields({ name: 'Uptime: ', value: `${uptime}%`, inline: true})
+      .addFields({ name: 'Ping: ', value: (ping / 5).toFixed(2) + "ms", inline: true})
       .setFooter({ text: 'Created By _roomysteve12'})
 
       inter.reply({ embeds: [embed] })
@@ -43,12 +48,10 @@ run: async (client, inter, config, db) => {
   }
 
   if(config.software === 'java') {
-   let { motd, players, online, version, info } = await fetch(`https://api.mcsrvstat.us/3/${config.ip}:${config.port}`).then(response => response.json()) 
+   let { motd, players, online, version } = await fetch(`https://api.mcstatus.io/v2/status/java/${config.ip}:${config.port}`).then(response => response.json()) 
     
-   let player = players
-   if(!player) player = "null"
-   if(!info) info = "null"
-   if(!player.list) player.list = "No Players"
+    if(!players) players = "No Players!"
+    let ping = Date.now() - start;
 
     if(online === false) {
       let embed = new Discord.EmbedBuilder()
@@ -69,13 +72,15 @@ run: async (client, inter, config, db) => {
       .setThumbnail(inter.guild.iconURL())
       .setTitle(`${config.emoji || ":skull:"} | ${inter.guild.name}'s Status`)
       .addFields({ name: 'Status: ', value: ':green_circle: Online', inline: true})
-      .addFields({ name: 'Players/Max: ', value: `${player.online}/${player.max}`, inline: true})
-      .addFields({ name: 'Players: ', value: `${player.list.map(item => item.name)}` || "Too Much Players!!"})
-      .addFields({ name: 'Version: ', value: `${version}`, inline: true})
-      .addFields({ name: 'Uptime (Not Accurate): ', value: `${uptime}%`, inline: true})
+      .addFields({ name: 'Players/Max: ', value: `${players.online}/${players.max}`, inline: true})
+      .addFields({ name: 'Players: ', value: `${players.list.map(item => item.name_clean)}` || "Too Many Players!"})
+      .addFields({ name: 'Version: ', value: `${version.name_clean}`, inline: true})
+      .addFields({ name: 'Uptime: ', value: `${uptime}%`, inline: true})
+      .addFields({ name: 'Ping: ', value: (ping / 5).toFixed(2) + "ms", inline: true})
       .setFooter({ text: 'Created By _roomysteve12'})
 
       inter.reply({ embeds: [embed] })
+      console.log(players)
     }
   }
   }
